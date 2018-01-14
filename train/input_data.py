acfc4df7e86a27f7ef2e70a1453431f631b3365c
Hanrui Wang <hrwang14@fudan.edu.cn>
2018-01-14 18:38:18 +0800

    fix a bug in inpu_data.py, no need to import data_augment module

--- train/input_data.py
+++ train/input_data.py
@@ -19,8 +19,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from data_augment import *
-
 import hashlib
 import math
 import os.path
