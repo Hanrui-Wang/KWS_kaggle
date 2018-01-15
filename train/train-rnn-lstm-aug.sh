34996a145a5df8bf8dba2e0f03c5d075dbacab32
kipsora <kipsora@gmail.com>
2018-01-15 18:41:56 +0800

    Training script for lstm changed.

--- train/train-rnn-lstm-aug.sh
+++ train/train-rnn-lstm-aug.sh
@@ -1,17 +1,22 @@
 python2 train.py \
 --data_url='' \
 --data_dir='./dataset' \
---summaries_dir='./summary/ml-lstm-aug' \
---train_dir='./model/ml-lstm-aug' \
+--summaries_dir='./summary/ml-lstm-aug-noise' \
+--train_dir='./model/ml-lstm-aug-noise' \
 --model_architecture='multilayer_lstm' \
 --batch_size=100 \
---learning_rate='0.0005,0.0001,0.00005' \
---how_many_training_steps='1500,5000,8000' \
+--learning_rate_start=0.0005 \
+--learning_rate_decay=0.5 \
+--epochs=20000 \
+--restore_step_interval=800 \
 --stretch=0.3 \
 --time_shift_ms=150 \
 --background_frequency=0.9 \
 --background_volume=0.18 \
---unknown_percentage=25.0 \
+--unknown_percentage=15.0 \
 --dct_coefficient_count=40 \
 --window_size_ms=25 \
 --window_stride_ms=10 \
+--weight_noise_stddev_start=0.001 \
+--weight_noise_stddev_decay=0.2 \
+--eval_step_interval=100 \
